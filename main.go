c3d16052313929198250bd90dbcc21eb5cd64b00
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2020-11-23 22:05:56 +0900

    Create main.go server.go
    Created data structure CachingData
    Created map Cache
    Modified Invoke( ) function for API caching

---
+++ main.go
@@ -0,0 +1,50 @@
+
+package main
+  
+import (
+        "log"
+
+        "github.com/keti-openfx/openfx/config"
+        "k8s.io/client-go/kubernetes"
+        "k8s.io/client-go/rest"
+
+        //ryan 
+        "time"
+)
+
+
+type CachingData struct{
+        serviceName string
+        lastCalled time.Time
+        c_input string
+        c_output string
+}
+
+var Cache = map[string]CachingData{}
+
+func main() {
+
+        /* kubernetes가 pod에 제공하는 config 객체를 반환
+         * kubernetes에서 실행중인 Pod 내부에서 실행하지 않으면, 에러 발생 */
+        conf, err := rest.InClusterConfig()
+        if err != nil {
+                log.Panic(err)
+        }
+        log.Printf("Kubernetes Host: %s\n", conf.Host)
+
+        /* 주어진 config을 위한 clienteset 반환 */
+        clientset, err := kubernetes.NewForConfig(conf)
+        if err != nil {
+                log.Panic(err)
+        }
+
+        /* FxGateway config 생성 */
+        c := config.NewFxGatewayConfig(Version)
+
+        /* FxGateway 생성 */
+        s := NewFxGateway(c, clientset)
+
+        /* FxGateway 실행 */
+        log.Printf("OpenFx Gateway Service Starting...\n")
+        s.Start()
+}
