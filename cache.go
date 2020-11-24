c3bddf4564a4c2388b90b63598697b1c39cffff3
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2020-11-24 12:51:51 +0900

    Create cache.go
    Implemented Cache structure as a class

---
+++ cache.go
@@ -0,0 +1,61 @@
+import (
+	"time"
+	"fmt"
+)
+
+type CachingData struct{
+	serviceName string
+	lastCalled time.Time
+	c_input string
+	c_output string
+}
+
+type CacheMap struct{
+	cmap map[string]CachingData
+}
+
+
+/////
+
+func NewCache() CacheMap {
+	cm := CacheMap{cmap: map[string]CachingData{} }
+	return cm
+}
+
+
+func (cm CacheMap) CacheIn(name string, cdata CachingData) {
+	cm.cmap[name] = cdata
+}
+
+func (cm CacheMap) CacheGet(name string) {
+	data, ok := cm.cmap[name]
+	if ok {
+			fmt.Println(data)
+	} else {
+			fmt.Println("No such data")
+	}
+}
+
+/*
+func main(){
+	Cache := NewCache()
+
+	cdata := CachingData{
+			serviceName: "Service name",
+			lastCalled: time.Now(),
+			c_input: "Input data",
+			c_output: "Output data",
+	}
+
+	fmt.Println(Cache)
+	fmt.Println(cdata)
+
+	fmt.Println()
+	Cache.CacheIn("Service name", cdata)
+	Cache.CacheGet("Service name")
+	Cache.CacheGet("Wrong name")
+
+	fmt.Println()
+	fmt.Println("done")
+}
+*/
